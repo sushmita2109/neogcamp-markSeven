f1329c1fbe2d96cabf37ce7dab5cf76bf37a4762
Sushmita <sushmitakumari363@gmail.com>
2022-08-30 10:59:33 +0530

    feat-script added

--- app.js
+++ app.js
@@ -3,3 +3,22 @@ var inputtxt=document.querySelector('#txt-input');
 var outputDiv=document.querySelector('#output-div');
 
 var serverurl='https://api.funtranslations.com/translate/dothraki.json';
+
+function getTranslationUrl(text){
+    return serverurl+"?"+"text="+text;
+}
+
+function errorHandler(error){
+    alert('Something is wrong with server');
+}
+
+function clickHandler(){
+    var inputText=inputtxt.value;
+    
+    fetch(getTranslationUrl(inputText))
+    .then(resonse=>response.json)
+    .then(json=>{
+        var translatedText=json.content.translated;
+        outputDiv.innerHTML=translatedText;
+    })
+}
