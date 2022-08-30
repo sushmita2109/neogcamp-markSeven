9f50561c7e61f24c49b0d1939aecd25ca4cb9f20
Sushmita <sushmitakumari363@gmail.com>
2022-08-30 10:32:02 +0530

    feat-inputtags added

---
+++ app.js
@@ -0,0 +1,5 @@
+var btnClicked=document.querySelector('#btn-Clicked');
+var inputtxt=document.querySelector('#txt-input');
+var outputDiv=document.querySelector('#output-div');
+
+var serverurl='https://api.funtranslations.com/translate/dothraki.json';
